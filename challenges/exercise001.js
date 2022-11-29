8f3395c779be6a6fa3e3747eb8a584f8bc79ac3e
Peter Viegas <peterviegas@outlook.com>
2022-11-29 16:31:27 +0000

    Change function generateInitials

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -8,7 +8,8 @@ export function capitalize(word) {
 export function generateInitials(firstName, lastName) {
     if (firstName === undefined) throw new Error("firstName is required");
     if (lastName === undefined) throw new Error("lastName is required");
-    // Add your code here!
+    console.log(firstName[0] + "." + lastName[0])
+    return firstName[0] + "." + lastName[0]
 }
 
 export function addVAT(originalPrice, vatRate) {
