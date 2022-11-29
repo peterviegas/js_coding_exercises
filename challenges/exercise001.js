c3622878753df506a2ea402ce64cebbfae160285
Peter Viegas <peterviegas@outlook.com>
2022-11-29 21:55:00 +0000

    change function simpleFizzBuzz

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -73,5 +73,14 @@ export function getMeanScore(scores) {
 
 export function simpleFizzBuzz(n) {
     if (n === undefined) throw new Error("n is required");
-    // Add your code here!
+
+    if (n % 3 == 0 && n % 5 == 0) {
+        return 'fizzbuzz';
+    } else if (n % 3 == 0) {
+        return 'fizz';
+    } else if (n % 5 == 0) {
+        return 'buzz';
+    } else {
+        return n;
+    }
 }
