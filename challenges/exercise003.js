b531041c13d134835eb65b6fb41ae9689520d869
Peter Viegas <peterviegas@outlook.com>
2022-11-30 18:54:20 +0000

    change function getSquares

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,26 +1,35 @@
 export function getSquares(nums) {
-  if (nums === undefined) throw new Error("nums is required");
-  // Your code here!
+    if (nums === undefined) throw new Error("nums is required");
+    const numsDouble = [];
+
+    for (var i = 0; i < nums.length; i++) {
+        numsDouble.push(nums[i] * nums[i]);
+    }
+
+    return numsDouble
 }
 
 export function camelCaseWords(words) {
-  if (words === undefined) throw new Error("words is required");
-  // Your code here!
+    if (words === undefined) throw new Error("words is required");
+
+    for (var i = 0; i < words.length; i++) {
+
+    }
 }
 
 export function getTotalSubjects(people) {
-  if (people === undefined) throw new Error("people is required");
-  // Your code here!
+    if (people === undefined) throw new Error("people is required");
+    // Your code here!
 }
 
 export function checkIngredients(menu, ingredient) {
-  if (menu === undefined) throw new Error("menu is required");
-  if (!ingredient) throw new Error("ingredient is required");
-  // Your code here!
+    if (menu === undefined) throw new Error("menu is required");
+    if (!ingredient) throw new Error("ingredient is required");
+    // Your code here!
 }
 
 export function duplicateNumbers(arr1, arr2) {
-  if (arr1 === undefined) throw new Error("arr1 is required");
-  if (arr2 === undefined) throw new Error("arr2 is required");
-  // Your code here!
+    if (arr1 === undefined) throw new Error("arr1 is required");
+    if (arr2 === undefined) throw new Error("arr2 is required");
+    // Your code here!
 }
