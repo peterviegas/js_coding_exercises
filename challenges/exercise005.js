13d97185fee86a6979c05a239e633a4b3f9e9fd4
Peter Viegas <peterviegas@outlook.com>
2022-12-02 22:17:24 +0000

    change function count1sand0s

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -18,7 +18,18 @@ export const findNextNumber = (nums, n) => {
 
 export const count1sand0s = (str) => {
     if (str === undefined) throw new Error("str is required");
-    // Your code here!
+
+    const frequencies = { 1: 0, 0: 0 };
+
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i];
+        if (frequencies[char] === undefined) {
+            frequencies[char] = 1;
+        } else {
+            frequencies[char] += 1;
+        }
+    }
+    return (frequencies);
 };
 
 export const reverseNumber = (n) => {
