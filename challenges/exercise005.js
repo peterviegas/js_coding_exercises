860b1b392038beb7a2880a581636be501afc00f4
Peter Viegas <peterviegas@outlook.com>
2022-12-03 03:15:45 +0000

    change function reverseNumber

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -34,7 +34,7 @@ export const count1sand0s = (str) => {
 
 export const reverseNumber = (n) => {
     if (n === undefined) throw new Error("n is required");
-    // Your code here!
+    return parseInt(n.toString().split('').reverse().join(''));
 };
 
 export const sumArrays = (arrs) => {
