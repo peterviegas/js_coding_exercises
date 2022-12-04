52fb52117fe002f369e77cd25493ddc789095cf3
Peter Viegas <peterviegas@outlook.com>
2022-12-04 22:36:30 +0000

    change function addressLookUp - Observation (user?.address?.postcode) not work in my Visual Code

--- challenges/exercise008-bonus-activity.js
+++ challenges/exercise008-bonus-activity.js
@@ -2,25 +2,23 @@
 
 export function printColours(colours) {
     if (!colours) throw new Error("colour is required");
-
     return [...colours];
 }
 
 export function shoppingList(list, newItem) {
     if (!list) throw new Error("list is required");
     if (!newItem) throw new Error("newItem is required");
-
     return [...list, ...newItem];
 }
 
 export function highestNumber(numbers) {
     if (!numbers) throw new Error("numbers are required");
-    // Your solution using the spread operator here!
+    return Math.max(...numbers);
 }
 
 export function splitThatString(string) {
     if (!string) throw new Error("string is required");
-    // Your solution using the spread operator here!
+    return string.split('');
 }
 
 // Optional Chaining Bonus activity!
@@ -28,4 +26,12 @@ export function splitThatString(string) {
 export function addressLookUp(user) {
     if (!user) throw new Error("user is required");
     // Your solution using optional chaining here!
+
+    let postcode = undefined;
+    if (user.address !== undefined) {
+        if (user.address.postcode !== undefined) {
+            postcode = user.address.postcode;
+        }
+    }
+    return postcode;
 }
